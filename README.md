a711929f74c1f2fbb4d739522cd2ad327e54c6a7
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-03-19 13:52:44 -0500

    refactor: Clean up code and documentation

--- README.md
+++ README.md
@@ -1,12 +1,44 @@
 # whisper-sandbox
 
-This is just a sandbox for me to try out Whisper (https://openai.com/research/whisper).
+<h1>Overview</h1>
 
-**Select an mp3 file** or **paste a link to a YouTube video** with Korean or Japanese-language audio and the program will transcribe the audio into the transcription field.
+This program is a GUI application that transcribes audio from an MP3 file or a YouTube video using the Whisper ASR model. It also provides translation functionality for the transcribed text.
 
-(Other languages work, but aren't officially supported in this project.)
+<h1>Dependencies</h1>
 
-Highlight any text in the transcription field at it will be dynamically translated in the translation field below.
+<p>
+whisper 
+<p>
+tkinter 
+<p>
+googletrans 
+<p>
+yt-dlp 
+<p>
+tempfile 
+<p>
+os
+<p>
+
+<h1>Functions</h1>
+
+download_audio_from_youtube(url): Downloads audio from a YouTube video and returns the path of the downloaded file.
+
+select_file(): Opens a file dialog to select an MP3 file, transcribes the audio, and displays the transcribed text.
+
+process_youtube_link(): Processes the YouTube link
+
+<h1>User Instructions</h1>
+
+Select an mp3 file or paste a link to a YouTube video with Korean or Japanese-language audio and the program will transcribe the audio into the transcription field.
+
+(Other languages may work, but aren't officially supported for this application.)
+
+Highlight any text in the transcription field at it will be translated dnyamically in the translation field below.
+
+<h1>Credit</h1>
+
+Thank you for using this application!
 
 Author: Adam Martinez
 Date: March 18, 2023
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from tkinter import filedialog
 from googletrans import Translator
 import yt_dlp
 import tempfile
+import os
 
+# Function to download audio from YouTube
 def download_audio_from_youtube(url):
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -21,12 +23,23 @@ def download_audio_from_youtube(url):
         temp_file = ydl.prepare_filename(info).replace('.webm', '.mp3')
         return temp_file
 
+# Initialize the tkinter window
 window = tk.Tk()
 window.title("whisper-sandbox")
 window.geometry("750x750")
 
 translator = Translator()
 
+# Function to select a file and transcribe it
+def select_file():
+    file_path = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("mp3 files", "*.mp3"),))
+    model = whisper.load_model("medium")
+    result = model.transcribe(file_path)
+    text = result["text"]
+    output_text.delete('1.0', tk.END)
+    output_text.insert(tk.END, text)
+
+# Function to process a YouTube link and transcribe the audio
 def process_youtube_link():
     url = youtube_url_entry.get()
     if url:
@@ -37,12 +50,17 @@ def process_youtube_link():
         output_text.delete('1.0', tk.END)
         output_text.insert(tk.END, text)
 
+# Function to translate the selected text
 def translate_text(event):
     selected_text = output_text.selection_get()
     if selected_text:
-            translated = translator.translate(selected_text, dest='en')
-            translation_text.delete('1.0', tk.END)
-            translation_text.insert(tk.END, translated.text)
+        translated = translator.translate(selected_text, dest='en')
+        translation_text.delete('1.0', tk.END)
+        translation_text.insert(tk.END, translated.text)
+
+# Create and configure tkinter widgets
+select_file_button = tk.Button(window, text="Select MP3 file", command=select_file)
+select_file_button.pack()
 
 youtube_url_label = tk.Label(window, text="Enter YouTube video URL")
 youtube_url_label.pack()
